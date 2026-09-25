78f18272981679c2f44ad868f829f7237c8df395
Kuldeep Dhangad <kuldeepdhangad6@gmail.com>
2026-09-25 15:30:01 +0530

    Time: 2 ms (97.44%), Space: 48.2 MB (67.78%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -2,45 +2,26 @@ class Solution {
     public int leastInterval(char[] tasks, int n) {
         int[] freq = new int[26];
 
-        for (char task : tasks) {
-            freq[task - 'A']++;
+        for (char c : tasks) {
+            freq[c - 'A']++;
         }
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int maxFreq = 0;
 
         for (int f : freq) {
-            if (f > 0) {
-                pq.add(f);
+            maxFreq = Math.max(maxFreq, f);
+        }
+
+        int countMax = 0;
+
+        for (int f : freq) {
+            if (f == maxFreq) {
+                countMax++;
             }
         }
 
-        int time = 0;
+        int ans = (maxFreq - 1) * (n + 1) + countMax;
 
-        while (!pq.isEmpty()) {
-            ArrayList<Integer> temp = new ArrayList<>();
-
-            int cycle = n + 1;
-
-            while (cycle > 0 && !pq.isEmpty()) {
-                int f = pq.poll();
-
-                if (f - 1 > 0) {
-                    temp.add(f - 1);
-                }
-
-                time++;
-                cycle--;
-            }
-
-            for (int f : temp) {
-                pq.add(f);
-            }
-
-            if (!pq.isEmpty()) {
-                time += cycle;
-            }
-        }
-
-        return time;
+        return Math.max(tasks.length, ans);
     }
 }
